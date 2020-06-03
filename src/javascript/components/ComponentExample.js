3b847656d6ba820456e63ec38db2f4c7d944ec07
Simon Patrat <simon.patrat@radio-canada.ca>
2020-06-03 11:53:01 -0400

    Add css variables Add css varaiables control from webcomponent

--- src/javascript/components/ComponentExample.js
+++ src/javascript/components/ComponentExample.js
@@ -1,21 +1,68 @@
 class ButtonComponent extends HTMLElement {
     constructor() {
         super();
+        this.handleClick = this.handleClick.bind(this);
         this.initElement();
+        this.savedColor = document.documentElement.style.getPropertyValue('--color-primary');
+        this.currentColor = null;
     }
-    
+
     initElement() {
         this.template = document.createElement('template');
         this.template.innerHTML = this.getTemplate();
         let shadowRoot = this.attachShadow({mode: 'open'});
         shadowRoot.appendChild(this.template.content.cloneNode(true));
+
     }
-    
+
+    connectedCallback() {
+        this.attachListeners();
+    }
+
+    disconnectedCallback() {
+       this.detachListeners();
+    }
+
     getTemplate() {
         return `
+            <style>
+                button {
+                    background: var(--color-custom-button-bg, blue);
+                    color: #fff;
+                    padding: 12px;
+                    border-radius: 20px;
+                    border: none;
+                    min-width: 120px;
+                }
+            </style>
             <button class="animation">Click me</button>
         `;
     }
+
+    getInnerElement() {
+        return this.template.content.querySelector('button');
+    }
+
+    attachListeners() {
+        const { shadowRoot } = this;
+        shadowRoot.querySelector('button').addEventListener('click', this.handleClick);
+    }
+
+    detachListeners() {
+        const { shadowRoot } = this;
+        shadowRoot.querySelector('button').removeEventListener('click', this.handleClick);
+    }
+
+    handleClick(event) {
+        if (this.currentColor  === 'blue') {
+            document.documentElement.style.setProperty('--color-primary', this.savedColor);
+            this.currentColor = this.savedColor;
+        } else {
+            document.documentElement.style.setProperty('--color-primary', 'blue');
+            this.currentColor = 'blue';
+        }
+    }
+
 }
 
 window.customElements.define('custom-button', ButtonComponent);
